ea10774bb5a22fb3c1ae27386d1c1f64372572f0
Chris Mansley <cmansley@cs.rutgers.edu>
2012-08-15 22:09:57 -0700

    Add installer

---
+++ installdotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# installdotfiles.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+
+# variables
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="bashrc bash_aliases.d bash_completion.d bash_functions.d emacs emacs.d"# list of files/folders to symlink in homedir
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/.$file ~/.$file
+done
